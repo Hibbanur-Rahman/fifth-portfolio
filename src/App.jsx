c47214f73d996eb44b6cbcf7ad3ff12752df511d
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-01-29 05:21:15 +0530

    adding the routes concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./views/About";
+import Contact from "./views/Contact";
+import Portfolio from "./views/Portfolio";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      {/* Include your navigation bar */}
+      <Navbar />
+      {/* Define your routes */}
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </Router>
   );
 }
 

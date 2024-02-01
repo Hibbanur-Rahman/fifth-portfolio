942b0c71c8b8a19c95afb37e3607d60dba67d72e
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-02-02 01:06:34 +0530

    change the font family and the change the icon height

--- src/components/profileSidebar.jsx
+++ src/components/profileSidebar.jsx
@@ -2,9 +2,9 @@ import profile from "../assets/images/profile-2.png";
 
 const ProfileSidebar = () => {
   return (
-    <div className="profileSidebar col-3 m-0 p-0 d-flex flex-column justify-content-center align-items-center position-relative mt-5">
+    <div className="profileSidebar col-12 col-md-3 m-0 p-0 d-flex flex-column justify-content-center align-items-center position-relative mt-5">
       <div className="profile col-8 rounded-4 position-absolute z-2 mt-5">
-        <img src={profile} alt="profile Image" className="rounded-4" />
+        <img src={profile} alt="profile" className="rounded-4" />
       </div>
       <div className="card m-0 p-5 col-12 d-flex flex-column justify-content-center align-items-center border-0 rounded-4">
         <h4 className="m-0 p-0 text-center ">Hibbanur Rahman</h4>
@@ -19,32 +19,31 @@ const ProfileSidebar = () => {
         </div>
         <div className="contact-details row m-0 p-3 mt-4 rounded-4 ">
           <div className="d-flex align-items-center ">
-            
-            <i class="fa-solid fa-mobile-screen"></i>
+            <i class="fa-solid fa-mobile-screen fa-1x p-3 bg-white rounded-2"></i>
             <div className="">
-                <p className="m-0 p-0">Phone</p>
-                <p className="">+91 99731 52523</p>
+              <p className="m-0 p-0">Phone</p>
+              <p className="">+91 99731 52523</p>
             </div>
           </div>
           <div className="d-flex align-items-center">
-            <i class="fa-solid fa-envelope-open-text"></i>{" "}
+            <i class="fa-solid fa-envelope-open-text fa-1x p-3 bg-white rounded-2"></i>
             <div className="">
-                <p className="m-0 p-0">Email</p>
-                <p className="">hibbanrahmanhyt@gmail.com</p>
+              <p className="m-0 p-0">Email</p>
+              <p className="">hibbanrahmanhyt@gmail.com</p>
             </div>
           </div>
           <div className="d-flex align-items-center">
-            <i class="fa-solid fa-location-dot"></i>
+            <i class="fa-solid fa-location-dot fa-1x p-3 bg-white rounded-2"></i>
             <div className="">
-                <p className="m-0 p-0">Location</p>
-                <p className="">Hyderabad,India</p>
+              <p className="m-0 p-0">Location</p>
+              <p className="">Hyderabad,India</p>
             </div>
           </div>
           <div className="d-flex align-items-center">
-            <i class="fa-regular fa-calendar-days"></i>
+            <i class="fa-regular fa-calendar-days fa-1x p-3 bg-white rounded-2"></i>
             <div className="">
-                <p className="m-0 p-0">Birthday</p>
-                <p className="">Feb 05,2004</p>
+              <p className="m-0 p-0">Birthday</p>
+              <p className="">Feb 05,2004</p>
             </div>
           </div>
         </div>

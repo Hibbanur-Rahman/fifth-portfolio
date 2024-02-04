df73a278d86ed3e7899e397d17dcbb666bf99669
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-02-04 13:51:10 +0530

    adding the functionality of dark mode and the light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useState,useEffect,React} from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./views/about";
 import Contact from "./views/contact";
@@ -9,15 +9,30 @@ import ProfileSidebar from "./components/profileSidebar";
 import MenuSidebar from "./components/menuSidebar";
 import Blogs from "./views/blogs";
 import Resume from "./views/resume";
+import bgLightImage from './assets/images/page-bg-light.jpg';
+import bgDarkImage from './assets/images/page-bg-dark.jpg';
+
+import './assets/styles/style.css';
 
 function App() {
+  const [isDark, setIsDark] = useState(true); // Declare isDark state
+  useEffect(() => {
+    // Update body background image when isDark state changes
+    const body = document.querySelector('body');
+    body.style.backgroundImage = isDark ? `url(${bgLightImage})` : `url(${bgDarkImage})`;
+
+    // Clean up function to remove inline style when component unmounts
+    return () => {
+      body.style.backgroundImage = '';
+    };
+  }, [isDark]);
   return (
     <Router>
       {/* Include your navigation bar */}
-      <Navbar />
-      {/* Define your routes */}
-      <div className="row m-0 p-0 p-3">
+      <Navbar isDark={isDark} setIsDark={setIsDark} /> {/* Pass isDark state and setIsDark function to Navbar */}   
+      <div className={`row m-0 p-0 p-3 ${isDark ? 'light-mode' : 'dark-mode'}`}>
         <ProfileSidebar />
+        
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/contact" element={<Contact />} />
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/logo-2.png";
+import '../assets/styles/style.css';
 
-const Navbar = () => {
+const Navbar = ({ isDark, setIsDark }) => { // Receive isDark and setIsDark as props
+  const handleClick = () => {
+    setIsDark(!isDark);
+  };
+
   return (
-    <div className="row m-0 p-0 justify-content-between p-5 mb-5">
+    <div className={`row m-0 p-0 justify-content-between p-5 mb-5 ${isDark ? 'navbar-dark' : 'navbar-light'}`}>
       <div className="col-2">
         <img src={logo} alt="logo" />
       </div>
-      <div className="w-auto rounded-circle bg-white p-3 pt-2 pb-2 m-0">
-        <i class="bi bi-moon m-0 p-0 fs-4"></i>
+      <div className={`w-auto rounded-circle ${isDark ? 'bg-dark' : 'bg-white'} p-3 pt-2 pb-2 m-0`} onClick={handleClick}>
+        <i className={`bi bi-moon m-0 p-0 fs-4 ${isDark ? 'text-white' : 'text-dark'}`}></i>
       </div>
     </div>
   );

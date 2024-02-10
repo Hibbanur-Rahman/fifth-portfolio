721c88130fcd375d0626632ee1ea8e8785b84c57
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-02-11 02:06:57 +0530

    adding the code in blogs

--- src/views/blogs.jsx
+++ src/views/blogs.jsx
@@ -1,26 +1,52 @@
-import '../assets/styles/style.css'
+import "../assets/styles/style.css";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const Blogs=()=>{
-    return (
-        <div className="about m-0 p-0 col-8  d-flex justify-content-center">
-      <div className="card m-0 p-0 bg-light rounded-4 col-11 border-0 ">
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Grid, Pagination } from 'swiper/modules';
+
+
+const Blogs = () => {
+  return (
+    <div className="blogs m-0 p-0 col-12 col-md-8  d-flex justify-content-center">
+      <div className="card rounded-4 m-0 p-2 pt-5 p-md-5 bg-light col-12 col-md-11 border-0 ">
         <div className="heading row m-0 p-0 d-flex align-items-center">
           <h1 className="m-0 p-0 w-auto">Blogs</h1>
           <div className="line bg-primary rounded-5 col-6 m-0 p-0 ms-3"></div>
         </div>
-        <p className="m-0 p-0">
-          I'm Creative Director and UI/UX Designer from Sydney, Australia,
-          working in web development and print media. I enjoy turning complex
-          problems into simple, beautiful and intuitive designs.
-        </p>
-        <p className="m-0 p-0">
-          My aim is to bring across your message and identity in the most
-          creative way. I created web design for many famous brand companies.
-        </p>
+        <div className="row m-0 p-0">
+          <Swiper
+            slidesPerView={3}
+            grid={{
+              rows: 2,
+            }}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Grid, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </div>
-    );
-}
-
+  );
+};
 
 export default Blogs;
--- src/views/contact.jsx
+++ src/views/contact.jsx
@@ -2,22 +2,54 @@ import "../assets/styles/style.css";
 
 const Contact = () => {
   return (
-    <div className="about m-0 p-0 col-12 col-md-8  d-flex justify-content-center">
+    <div className="contact m-0 p-0 col-12 col-md-8  d-flex justify-content-center">
       <div className="card rounded-4 m-0 p-2 pt-5 p-md-5 bg-light col-12 col-md-11 border-0 ">
         <div className="heading row m-0 p-0 d-flex align-items-center">
           <h1 className="m-0 p-0 w-auto">Contact</h1>
           <div className="line bg-primary rounded-5 col-6 m-0 p-0 ms-3"></div>
         </div>
 
-        <div className="col-12">
+        <div className="col-12 pt-5">
           <p className="m-0 p-0 fs-4 text-body-secondary ">
             I'm Always Open To Discussing Product
           </p>
           <h4 className="m-0 p-0">design work or partnerships.</h4>
-        <form action="" method="post">
-          
-        </form>
+          <form action="" method="post" className="mt-5">
+            <div className="form-group mb-3 mt-4">
+              <input
+                type="email"
+                className="form-control bg-transparent p-3 border border-top-0 border-start-0 border-end-0"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Name*"
+              />
+            </div>
+            <div className="form-group mb-3 mt-4">
+              <input
+                type="email"
+                className="form-control bg-transparent p-3 border border-top-0 border-start-0 border-end-0"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Email*"
+              />
+            </div>
+            <div className="form-group mb-3 mt-4">
+              <textarea
+                type="email"
+                className="form-control bg-transparent p-3 border border-top-0 border-start-0 border-end-0"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Message*"
+              />
+            </div>
 
+            <button
+              type="submit"
+              className="btn btn-lg mt-5 bg-transparent border border-2 "
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </div>

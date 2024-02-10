45d6b281e7ce0ce850d7637ff8958df6a360fd79
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-02-10 21:57:38 +0530

    adding the contact secton

--- src/views/contact.jsx
+++ src/views/contact.jsx
@@ -2,11 +2,22 @@ import "../assets/styles/style.css";
 
 const Contact = () => {
   return (
-    <div className="about m-0 p-0 col-8  d-flex justify-content-center">
-      <div className="card m-0 p-0 bg-light rounded-4 col-11 border-0 ">
+    <div className="about m-0 p-0 col-12 col-md-8  d-flex justify-content-center">
+      <div className="card rounded-4 m-0 p-2 pt-5 p-md-5 bg-light col-12 col-md-11 border-0 ">
         <div className="heading row m-0 p-0 d-flex align-items-center">
           <h1 className="m-0 p-0 w-auto">Contact</h1>
           <div className="line bg-primary rounded-5 col-6 m-0 p-0 ms-3"></div>
+        </div>
+
+        <div className="col-12">
+          <p className="m-0 p-0 fs-4 text-body-secondary ">
+            I'm Always Open To Discussing Product
+          </p>
+          <h4 className="m-0 p-0">design work or partnerships.</h4>
+        <form action="" method="post">
+          
+        </form>
+
         </div>
       </div>
     </div>
